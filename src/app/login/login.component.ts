9eefd4fdcacd2ef57e0909e53c51b5720a0a7db6
Mayank <mayank283@live.com>
2017-04-26 01:25:17 +0530

    4/26/2017

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from './login.service';
 import { User } from '../signup/user';
+import { Md5 } from 'ts-md5/dist/md5';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  providers: [LoginService]
+  providers: [LoginService,Md5]
 })
 export class LoginComponent {
 
--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -3,7 +3,7 @@ import { Http, RequestOptions, Response, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
-
+import { Md5 } from 'ts-md5/dist/md5';
 
 /**Import custom classes*/
 import { User } from '../signup/user';
@@ -11,27 +11,30 @@ import { User } from '../signup/user';
 @Injectable()
 export class LoginService {
 
-
   private loginUrl = 'http://localhost:8080/login';
 
-  constructor(private http: Http) { }
+  constructor(private http: Http,
+              private _md5: Md5) { }
 
-  login(user: User): Observable<User> {
+  login(user: User): Observable<any> {
+    let username: string = user.username;
+    let password: string = user.password;
     let headers = new Headers({ 'Content-Type': 'application/json' });
+    headers.append("Authorization", "Basic " + btoa(username + ":" + password));
+    let e = Md5.hashStr(password);
+    console.log(e);
     let options = new RequestOptions({ headers: headers });
-    console.log("It works");
-    console.log(user);
-    return this.http.post(this.loginUrl, user)
+    return this.http.get(this.loginUrl,options)
       .map(this.extractData)
       .catch(this.handleError);
   }
-  
+
   private extractData(res: Response) {
     console.log(res.json());
     return res.json();
   }
-  
-  private handleError (error: Response | any) {
+
+  private handleError(error: Response | any) {
     // In a real world app, you might use a remote logging infrastructure
     let errMsg: string;
     if (error instanceof Response) {
@@ -44,5 +47,4 @@ export class LoginService {
     console.error(errMsg);
     return Observable.throw(errMsg);
   }
-
 }
